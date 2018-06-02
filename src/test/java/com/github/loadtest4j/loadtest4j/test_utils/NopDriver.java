2f88fe44630d9b81d932a1e2fed70642dbbcdf22
themasterchef <themasterchef@users.noreply.github.com>
2018-06-02 21:24:37 +0100

    Include actual test duration in the result (#47)

--- src/test/java/com/github/loadtest4j/loadtest4j/test_utils/NopDriver.java
+++ src/test/java/com/github/loadtest4j/loadtest4j/test_utils/NopDriver.java
@@ -4,6 +4,7 @@ import com.github.loadtest4j.loadtest4j.Driver;
 import com.github.loadtest4j.loadtest4j.DriverRequest;
 import com.github.loadtest4j.loadtest4j.DriverResult;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,6 +24,11 @@ public class NopDriver implements Driver {
         @Override
         public long getKo() {
             return 0;
+        }
+
+        @Override
+        public Duration getActualDuration() {
+            return Duration.ZERO;
         }
 
         @Override
