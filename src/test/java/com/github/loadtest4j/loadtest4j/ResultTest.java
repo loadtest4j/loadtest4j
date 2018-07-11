b135e229f4bdec1676a24375780891eb4138299f
Chris Kilding <themasterchef@users.noreply.github.com>
2018-07-11 10:09:44 +0100

    Update ResultTest.java

--- src/test/java/com/github/loadtest4j/loadtest4j/ResultTest.java
+++ src/test/java/com/github/loadtest4j/loadtest4j/ResultTest.java
@@ -14,28 +14,28 @@ public class ResultTest {
 
     @Test
     public void testGetTotal() {
-        final Result result = new Result(1, 1, Duration.ZERO, new NopResponseTime());
+        final Result result = new Result(1, 1, Duration.ZERO, createResponseTime());
 
         assertThat(result.getTotal()).isEqualTo(2);
     }
 
     @Test
     public void testGetPercentKo() {
-        final Result result = new Result(0, 2, Duration.ZERO, new NopResponseTime());
+        final Result result = new Result(0, 2, Duration.ZERO, createResponseTime());
 
         assertThat(result.getPercentKo()).isEqualTo(100);
     }
 
     @Test
     public void testGetPercentKoAvoidsDivideByZero() {
-        final Result result = new Result(0, 0, Duration.ZERO, new NopResponseTime());
+        final Result result = new Result(0, 0, Duration.ZERO, createResponseTime());
 
         assertThat(result.getPercentKo()).isEqualTo(0);
     }
 
     @Test
     public void testGetPercentKoWithErrors() {
-        final Result result = new Result(3, 1, Duration.ZERO, new NopResponseTime());
+        final Result result = new Result(3, 1, Duration.ZERO, createResponseTime());
 
         assertThat(result.getPercentKo()).isEqualTo(25);
     }
@@ -45,36 +45,40 @@ public class ResultTest {
         final long ok = Long.MAX_VALUE / 2;
         final long ko = Long.MAX_VALUE / 2;
 
-        final Result result = new Result(ok, ko, Duration.ZERO, new NopResponseTime());
+        final Result result = new Result(ok, ko, Duration.ZERO, createResponseTime());
 
         assertThat(result.getPercentKo()).isEqualTo(50);
     }
 
     @Test
     public void testGetActualDuration() {
-        final Result result = new Result(1, 1, Duration.ofSeconds(2), new NopResponseTime());
+        final Result result = new Result(1, 1, Duration.ofSeconds(2), createResponseTime());
 
         assertThat(result.getActualDuration()).isEqualTo(Duration.ofSeconds(2));
     }
 
     @Test
     public void testGetRequestsPerSecond() {
-        final Result result = new Result(5, 5, Duration.ofMillis(2500), new NopResponseTime());
+        final Result result = new Result(5, 5, Duration.ofMillis(2500), createResponseTime());
 
         assertThat(result.getRequestsPerSecond()).isEqualTo(4);
     }
 
     @Test
     public void testGetRequestsPerSecondAvoidsDivideByZero() {
-        final Result result = new Result(2, 0, Duration.ZERO, new NopResponseTime());
+        final Result result = new Result(2, 0, Duration.ZERO, createResponseTime());
 
         assertThat(result.getRequestsPerSecond()).isEqualTo(0);
     }
 
     @Test
     public void testGetResponseTime() {
-        final Result result = new Result(2, 0, Duration.ZERO, new NopResponseTime());
+        final Result result = new Result(2, 0, Duration.ZERO, createResponseTime());
 
         assertThat(result.getResponseTime().getPercentile(50)).isEqualTo(Duration.ZERO);
+    }
+    
+    private static ResponseTime createResponseTime() {
+        return new ResponseTime(new NopResponseTime());
     }
 }
